071cae325bb4d45e4eea9e10dbb222f80c899c18
andresrojanoc <andresrojano@udec.cl>
2026-06-18 12:26:13 -0400

    inherit from FixTTMGrid

--- FixTTM_MMMG/fix/fix_ttm_cascade.h
+++ FixTTM_MMMG/fix/fix_ttm_cascade.h
@@ -20,58 +20,17 @@ FixStyle(ttm/cascade,FixTTMCascade);
 #ifndef LMP_FIX_TTM_CASCADE_H
 #define LMP_FIX_TTM_CASCADE_H
 
-#include "fix_ttm.h"
+#include "fix_ttm_grid.h"
 
 namespace LAMMPS_NS {
 
-class FixTTMCascade : public FixTTM {
+class FixTTMCascade : public FixTTMGrid {
  public:
   FixTTMCascade(class LAMMPS *, int, char **);
   ~FixTTMCascade() override;
-  void post_constructor() override;
-  void init() override;
   void post_force(int) override;
   void end_of_step() override;
-  void write_restart(FILE *) override;
-  void restart(char *) override;
-  void write_restart_file(const char *) override;
   double compute_vector(int) override;
-  double memory_usage() override;
-
-  // grid communication
-
-  void reset_grid() override;
-
-  void pack_forward_grid(int, void *, int, int *) override;
-  void unpack_forward_grid(int, void *, int, int *) override;
-  void pack_reverse_grid(int, void *, int, int *) override;
-  void unpack_reverse_grid(int, void *, int, int *) override;
-  void pack_remap_grid(int, void *, int, int *) override;
-  void unpack_remap_grid(int, void *, int, int *) override;
-  int unpack_read_grid(int, char *) override;
-  void pack_write_grid(int, void *) override;
-  void unpack_write_grid(int, void *, int *) override;
-
-  int get_grid_by_name(const std::string &, int &) override;
-  void *get_grid_by_index(int) override;
-  int get_griddata_by_name(int, const std::string &, int &) override;
-  void *get_griddata_by_index(int) override;
-
- private:
-  int ngridown, ngridout;
-  int nxlo_in, nxhi_in, nylo_in, nyhi_in, nzlo_in, nzhi_in;
-  int nxlo_out, nxhi_out, nylo_out, nyhi_out, nzlo_out, nzhi_out;
-  double skin_original;
-  FILE *fpout;
-
-  class Grid3d *grid;
-  class Grid3d *grid_previous;
-  double ***T_electron_previous;
-  int ngrid_buf1, ngrid_buf2;
-  double *grid_buf1, *grid_buf2;
-
-  double ***T_electron_read;
-  int nxlo_out_previous, nylo_out_previous, nzlo_out_previous;
 
  protected:
   bool cutoff_active, offset_active, cetable_active, ketable_active;
@@ -93,7 +52,6 @@ class FixTTMCascade : public FixTTM {
 
   void allocate_grid() override;
   void deallocate_grid() override;
-  void read_electron_temperatures(const std::string &) override;
 
   void tableinterpreader(const std::string &filename, const std::string &keyword);
   double linearinterpolation(double temp, const std::string &keyword);
